14fca901d9d05540b0c5352200c0e4f6727a2ab9
hezg@sunwayworld.com <hzgdeemail@163.com>
2022-02-23 11:34:45 +0800

    修改vite.config.ts的配置，完成打包环境自定义。

--- src/utils/request/config.ts
+++ src/utils/request/config.ts
@@ -1,7 +1,10 @@
 let BASE_URL = "";
-let GetImage_BASE_URL = "/full";
+let GetImage_BASE_URL = "";
 const TIME_OUT = 10000;
 // 导入环境变量
 BASE_URL = import.meta.env.VITE_APP_BASEURL as string;
+GetImage_BASE_URL = import.meta.env.VITE_GETIMAGE_BASEURL as string;
+console.log("GetImage_BASE_URL", GetImage_BASE_URL);
+
 
 export { BASE_URL, TIME_OUT, GetImage_BASE_URL };
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -12,48 +12,63 @@ function pathResolve(dir: string) {
   return resolve(process.cwd(), ".", dir);
 }
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    // 按需导入element-plus组件
-    Components({ resolvers: [ElementPlusResolver()] }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: /\@\//,
-        replacement: pathResolve("src") + "/",
+export default ({ mode }) => {
+  const envConfig = loadEnv(mode, process.cwd());
+  console.log("envConfig", envConfig);
+
+  return defineConfig({
+    build: {
+      assetsDir: 'static/img/',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        },
       },
+    },
+    plugins: [
+      vue(),
+      // 按需导入element-plus组件
+      Components({ resolvers: [ElementPlusResolver()] }),
     ],
-  },
-  // base: './',
-  base: "/one-wallhaven/",
-  css: {
-    preprocessorOptions: {
-      less: {
-        charset: false,
-        /*
-                  引入var.scss全局预定义变量，
-                  如果引入多个文件，
-                  可以使用
-                  '@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
-                  这种格式
-                   */
-        additionalData: `@import "./src/assets/styles/global.less";`,
-      }
+    resolve: {
+      alias: [
+        {
+          find: /\@\//,
+          replacement: pathResolve("src") + "/",
+        },
+      ],
     },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api/v1': {
-        target: 'https://wallhaven.cc',
-        changeOrigin: true,
+    base: envConfig.VITE_BASEURL,
+    css: {
+      preprocessorOptions: {
+        less: {
+          charset: false,
+          /*
+                    引入var.scss全局预定义变量，
+                    如果引入多个文件，
+                    可以使用
+                    '@import "@/assets/scss/globalVariable1.scss";@import "@/assets/scss/globalVariable2.scss";'
+                    这种格式
+                     */
+          additionalData: `@import "./src/assets/styles/global.less";`,
+        }
       },
-      '/full': {
-        target: 'https://w.wallhaven.cc',
-        changeOrigin: true,
-      }
     },
-  },
-});
+    server: {
+      port: 3000,
+      proxy: {
+        '/api/v1': {
+          target: 'https://wallhaven.cc',
+          changeOrigin: true,
+        },
+        '/full': {
+          target: 'https://w.wallhaven.cc',
+          changeOrigin: true,
+        }
+      },
+    },
+  });
+
+}
